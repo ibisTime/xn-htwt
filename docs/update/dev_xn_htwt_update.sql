c0a10a0ca086f1b948411b5e55078012e52b2e57
pyez1158 <pyez1158@163.com>
2018-06-22 19:17:15 +0800

    脚本整理

--- docs/update/dev_xn_htwt_update.sql
+++ docs/update/dev_xn_htwt_update.sql
@@ -1,6 +1,6 @@
-ALTER TABLE `dev_xn_htwt`.`tp_archive` 
+ALTER TABLE `tp_archive` 
 DROP COLUMN `is_delete`;
 
-ALTER TABLE `dev_xn_htwt`.`tp_social_relation` 
+ALTER TABLE `tp_social_relation` 
 DROP COLUMN `is_delete`;
 
