c445384c03cf0468ddf184e5e0be8787cb4a8162
ITessence <yunlai0821@163.com>
2018-11-01 10:16:15 +0800

    脚本整理

--- docs/update/v1.4.0_update_xn_htwt.sql
+++ docs/update/v1.4.0_update_xn_htwt.sql
@@ -1,2 +1,4 @@
 ALTER TABLE `tdq_budget_order` 
 ADD COLUMN `intev_cur_node_code` varchar(32) NULL COMMENT '面签节点编号' AFTER `cur_node_code`;
+
+UPDATE `tsys_node_flow` SET `next_node`='002_07' WHERE `id`='63';
