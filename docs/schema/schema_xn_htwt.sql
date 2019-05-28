6530c096376e54189c2153939ba11ce6760b9b67
cyl <chaiyunlai@lanchengiot.com>
2019-05-28 14:08:17 +0800

    垫资审核不通过，是否垫资改为否

--- docs/schema/schema_xn_htwt.sql
+++ docs/schema/schema_xn_htwt.sql
@@ -443,6 +443,20 @@ CREATE TABLE `tdh_repoint` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='返点表';
 /*!40101 SET character_set_client = @saved_cs_client */;
 
+DROP TABLE IF EXISTS `tdh_repoint_account`;
+CREATE TABLE `tdh_repoint_account`
+(
+  `code`              varchar(32) NOT NULL COMMENT '编号',
+  `repoint_code`      varchar(32)  DEFAULT NULL COMMENT '返点编号',
+  `repoint_card_code` varchar(32)  DEFAULT NULL COMMENT '返点账号编号',
+  `actual_amount`     bigint(20)   DEFAULT NULL COMMENT '实返金额',
+  `water_bill`        text         DEFAULT NULL COMMENT '水单',
+  `updater`           varchar(255) DEFAULT NULL COMMENT '更新人',
+  `update_datetime`   datetime     DEFAULT NULL COMMENT '更新时间',
+  `remark`            varchar(255) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`code`)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8 COMMENT ='返点账号列表';
+
 --
 -- Table structure for table `tdq_advance`
 --
