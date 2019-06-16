c3d63b1c6f63ee42dc94425edd2510796cb494bf
tao <taojian@192.168.1.186>
2019-06-16 15:41:25 +0800

    同盾征信

--- src/main/java/com/cdkj/loan/bo/impl/CreditUserBOImpl.java
+++ src/main/java/com/cdkj/loan/bo/impl/CreditUserBOImpl.java
@@ -383,8 +383,9 @@ public class CreditUserBOImpl extends PaginableBOImpl<CreditUser> implements
         req.setCompanyCode("CD-TDUN00030");
         try {
             result= BizConnecter.getBizData("798600", JsonUtils.object2Json(req));
+            result=result.replace("\"","");
               //转json储存在数据库
-              creditUser.setTongdunResult(JsonUtil.Object2Json(result));
+              creditUser.setTongdunResult(result);
               creditUserDAO.updateTongdun(creditUser);
         }catch (Exception e) {
             logger.info("同盾结果返回异常");
